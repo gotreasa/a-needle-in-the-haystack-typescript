c93a51714a91f64b9f9b7678c967e7bafadee85c
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-01 13:09:03 +0000

    refactor: use one variable for the message

--- src/NeedleFinder.ts
+++ src/NeedleFinder.ts
@@ -4,13 +4,15 @@ const hasMoreThanOneNeedle = (input: string[]): boolean =>
   input.indexOf('needle') !== input.lastIndexOf('needle');
 
 const findNeedle = (input: string[]): string => {
-  if (hasNoNeedle(input)) return 'no needle found';
+  let message;
+  if (hasNoNeedle(input)) message = 'no needle found';
 
-  if (hasMoreThanOneNeedle(input)) return 'error';
+  if (hasMoreThanOneNeedle(input)) message = 'error';
 
   const position = input.indexOf('needle');
+  message = `found the needle at position ${position}`;
 
-  return `found the needle at position ${position}`;
+  return message;
 };
 
 export default findNeedle;
